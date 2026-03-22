506cadcb53b2eaac93be2965b901ed7c210fc5a4
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-23 06:51:02 +0700

    Update ArisTeam.java

--- me/vennlmao/aristeam/ArisTeam.java
+++ me/vennlmao/aristeam/ArisTeam.java
@@ -12,18 +12,23 @@ public class ArisTeam extends JavaPlugin {
         saveDefaultConfig();
         saveResource("messages.yml", false);
         saveResource("gui.yml", false);
+        
         this.msgManager = new MessageManager(this);
         this.teamManager = new TeamManager(this);
         this.tpManager = new TeleportManager(this);
+
         if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             new TeamExpansion(this).register();
         }
+
         TeamCommand cmd = new TeamCommand(this);
         getCommand("team").setExecutor(cmd);
         getCommand("team").setTabCompleter(cmd);
+        
         getServer().getPluginManager().registerEvents(new InventoryListener(this), this);
     }
+
     public MessageManager getMsgManager() { return msgManager; }
     public TeamManager getTeamManager() { return teamManager; }
     public TeleportManager getTeleportManager() { return tpManager; }
-  }
+}
