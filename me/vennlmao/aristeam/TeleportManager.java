c3c280e1e057b9a62132e1aebf29c8891abb4269
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-23 06:54:07 +0700

    Update TeleportManager.java

--- me/vennlmao/aristeam/TeleportManager.java
+++ me/vennlmao/aristeam/TeleportManager.java
@@ -1,6 +1,7 @@
 package me.vennlmao.aristeam;
 
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -9,21 +10,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TeleportManager {
     private final ArisTeam plugin;
     public TeleportManager(ArisTeam plugin) { this.plugin = plugin; }
+
     public void startTeleport(Player p, Location loc) {
-        Location start = p.getLocation();
-        AtomicInteger count = new AtomicInteger(5);
+        Location startPos = p.getLocation();
+        AtomicInteger seconds = new AtomicInteger(plugin.getConfig().getInt("settings.teleport-delay"));
+
         plugin.getServer().getAsyncScheduler().runAtFixedRate(plugin, (task) -> {
-            if (!p.isOnline() || p.getLocation().distance(start) > 0.1) {
+            if (!p.isOnline() || p.getLocation().distance(startPos) > plugin.getConfig().getDouble("settings.teleport-cancel-distance")) {
                 plugin.getMsgManager().send(p, "teleport-cancelled", null);
+                p.playSound(p.getLocation(), Sound.valueOf(plugin.getConfig().getString("sounds.teleport-fail")), 1f, 1f);
                 task.cancel();
                 return;
             }
-            int cur = count.getAndDecrement();
-            if (cur > 0) plugin.getMsgManager().send(p, "teleporting", Map.of("%time%", String.valueOf(cur)));
-            else {
-                p.teleportAsync(loc).thenAccept(s -> plugin.getMsgManager().send(p, "teleport-success", null));
+
+            int current = seconds.getAndDecrement();
+            if (current > 0) {
+                plugin.getMsgManager().send(p, "teleporting", Map.of("%time%", String.valueOf(current)));
+                p.playSound(p.getLocation(), Sound.valueOf(plugin.getConfig().getString("sounds.teleport-tick")), 1f, 1f);
+            } else {
+                p.teleportAsync(loc).thenAccept(success -> {
+                    if (success) {
+                        plugin.getMsgManager().send(p, "teleport-success", null);
+                        p.playSound(p.getLocation(), Sound.valueOf(plugin.getConfig().getString("sounds.teleport-success")), 1f, 1f);
+                    }
+                });
                 task.cancel();
             }
         }, 0L, 1L, TimeUnit.SECONDS);
     }
-                                                }
+        }
