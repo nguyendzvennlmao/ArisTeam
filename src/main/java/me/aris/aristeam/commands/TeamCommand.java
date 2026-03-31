f5bcfedda375db1f647d41ee112a272e4752eb1c
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 06:18:20 +0700

    Update TeamCommand.java

--- src/main/java/me/aris/aristeam/commands/TeamCommand.java
+++ src/main/java/me/aris/aristeam/commands/TeamCommand.java
@@ -4,10 +4,8 @@ import me.aris.aristeam.ArisTeams;
 import me.aris.aristeam.manager.MenuManager;
 import me.aris.aristeam.manager.TeamManager;
 import me.aris.aristeam.utils.MessageUtils;
-import me.aris.aristeam.utils.ColorUtils;
 import me.aris.aristeam.utils.TeleportHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import java.util.*;
@@ -18,61 +16,57 @@ public class TeamCommand implements CommandExecutor, TabCompleter {
         if (!(s instanceof Player)) return true;
         Player p = (Player) s;
         TeamManager tm = ArisTeams.getInstance().getTeamManager();
+
         if (args.length == 0) {
             if (tm.hasTeam(p)) new MenuManager().openMain(p);
             else MessageUtils.sendMessage(p, "not-in-team");
             return true;
         }
+
         switch (args[0].toLowerCase()) {
             case "create":
                 if (args.length < 2) return true;
                 if (tm.hasTeam(p)) { MessageUtils.sendMessage(p, "already-in-team"); return true; }
                 tm.createTeam(p, args[1]);
-                MessageUtils.sendMessage(p, "team-created", "%team%", args[1]);
-                p.playSound(p.getLocation(), Sound.valueOf(ArisTeams.getInstance().getConfig().getString("sounds.team-create")), 1f, 1f);
-                break;
-            case "sethome":
-                if (!tm.hasTeam(p)) { MessageUtils.sendMessage(p, "not-in-team"); return true; }
-                if (!tm.isOwner(p)) { MessageUtils.sendMessage(p, "no-permission"); return true; }
-                tm.getTeam(p).home = p.getLocation();
-                MessageUtils.sendMessage(p, "home-set");
-                break;
-            case "home":
-                if (tm.hasTeam(p) && tm.getTeam(p).home != null) {
-                    TeleportHandler.startTeleport(p, tm.getTeam(p).home);
-                } else if (tm.hasTeam(p)) {
-                    MessageUtils.sendMessage(p, "home-not-set");
-                }
-                break;
-            case "ec":
-                if (tm.hasTeam(p)) p.openInventory(tm.getTeam(p).ec);
-                else MessageUtils.sendMessage(p, "not-in-team");
-                break;
-            case "kick":
-                if (args.length < 2) return true;
-                if (tm.isOwner(p)) new MenuManager().openConfirm(p, "kick", args[1]);
+                MessageUtils.sendMessage(p, "team-created");
                 break;
             case "invite":
                 if (args.length < 2) return true;
+                if (!tm.isOwner(p)) { MessageUtils.sendMessage(p, "no-permission"); return true; }
                 Player target = Bukkit.getPlayer(args[1]);
-                if (target != null && tm.hasTeam(p)) {
+                if (target != null) {
                     tm.addInvite(target.getUniqueId(), tm.getTeam(p).name);
+                    MessageUtils.sendMessage(p, "invite-sent", "%player%", target.getName());
                     MessageUtils.sendMessage(target, "invite-received", "%team%", tm.getTeam(p).name);
-                    MessageUtils.sendMessage(p, "invite-send", "%player%", target.getName());
                 }
                 break;
             case "join":
                 if (args.length < 2) return true;
+                if (tm.hasTeam(p)) { MessageUtils.sendMessage(p, "already-in-team"); return true; }
                 if (tm.getInvites(p.getUniqueId()).contains(args[1])) {
                     tm.joinTeam(p, args[1]);
-                    MessageUtils.sendMessage(p, "player-joined");
-                } else { MessageUtils.sendMessage(p, "invalid-team"); }
+                    MessageUtils.sendMessage(p, "joined-team", "%team%", args[1]);
+                } else { MessageUtils.sendMessage(p, "no-invite"); }
+                break;
+            case "sethome":
+                if (tm.isOwner(p)) {
+                    tm.getTeam(p).home = p.getLocation();
+                    MessageUtils.sendMessage(p, "home-set");
+                }
+                break;
+            case "home":
+                if (tm.hasTeam(p) && tm.getTeam(p).home != null) {
+                    TeleportHandler.startTeleport(p, tm.getTeam(p).home);
+                }
+                break;
+            case "ec":
+                if (tm.hasTeam(p)) p.openInventory(tm.getTeam(p).ec);
+                break;
+            case "kick":
+                if (args.length >= 2 && tm.isOwner(p)) new MenuManager().openConfirm(p, "kick", args[1]);
                 break;
             case "disband":
                 if (tm.isOwner(p)) new MenuManager().openConfirm(p, "disband", null);
-                break;
-            case "leave":
-                if (tm.hasTeam(p)) new MenuManager().openConfirm(p, "leave", null);
                 break;
             case "reload":
                 if (p.hasPermission("aristeams.admin")) {
@@ -87,10 +81,7 @@ public class TeamCommand implements CommandExecutor, TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender s, Command c, String l, String[] a) {
         if (a.length == 1) return Arrays.asList("create", "join", "invite", "disband", "leave", "home", "sethome", "ec", "kick", "reload");
-        if (a.length == 2) {
-            if (a[0].equalsIgnoreCase("join")) return ArisTeams.getInstance().getTeamManager().getInvites(((Player)s).getUniqueId());
-            if (a[0].equalsIgnoreCase("kick") || a[0].equalsIgnoreCase("invite")) return null;
-        }
+        if (a.length == 2 && a[0].equalsIgnoreCase("join")) return ArisTeams.getInstance().getTeamManager().getInvites(((Player)s).getUniqueId());
         return new ArrayList<>();
     }
-                }
+        }
