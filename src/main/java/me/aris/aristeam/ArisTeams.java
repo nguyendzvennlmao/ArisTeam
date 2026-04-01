069307bc7d570df7af9d48e3fdffe391eb6a84fb
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 07:33:18 +0700

    Update ArisTeams.java

--- src/main/java/me/aris/aristeam/ArisTeams.java
+++ src/main/java/me/aris/aristeam/ArisTeams.java
@@ -1,20 +1,50 @@
 package me.aris.aristeam;
 
 import me.aris.aristeam.manager.TeamManager;
+import me.aris.aristeam.manager.MenuManager;
+import me.aris.aristeam.listeners.MenuListener;
+import me.aris.aristeam.listeners.DamageListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArisTeams extends JavaPlugin {
     private static ArisTeams instance;
     private TeamManager teamManager;
+    private MenuManager menuManager;
 
     @Override
     public void onEnable() {
         instance = this;
+        
         saveDefaultConfig();
-        teamManager = new TeamManager();
-        teamManager.loadData();
+        
+        this.teamManager = new TeamManager();
+        this.menuManager = new MenuManager();
+        
+        this.teamManager.loadData();
+        
+        Bukkit.getPluginManager().registerEvents(new MenuListener(), this);
+        Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
+        
+        getCommand("team").setExecutor(new me.aris.aristeam.commands.TeamCommand());
     }
 
-    public static ArisTeams getInstance() { return instance; }
-    public TeamManager getTeamManager() { return teamManager; }
-}
+    @Override
+    public void onDisable() {
+        if (teamManager != null) {
+            teamManager.saveData();
+        }
+    }
+
+    public static ArisTeams getInstance() {
+        return instance;
+    }
+
+    public TeamManager getTeamManager() {
+        return teamManager;
+    }
+
+    public MenuManager getMenuManager() {
+        return menuManager;
+    }
+    }
