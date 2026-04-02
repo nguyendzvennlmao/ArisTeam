7f45daa05f3b98a84c81adabf25a576a58fed701
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-02 19:24:42 +0700

    Create ActionBarTask.java

---
+++ src/main/java/me/aris/aristeam/ActionBarTask.java
@@ -0,0 +1,35 @@
+package me.aris.aristeam;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class ActionBarTask extends BukkitRunnable {
+    private Aristeam plugin;
+
+    public ActionBarTask(Aristeam plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        for (Player p : plugin.getServer().getOnlinePlayers()) {
+            Team team = plugin.getTeamManager().getPlayerTeam(p.getUniqueId());
+            if (team != null) {
+                String pvpStatus = plugin.getConfigManager().getPvpStatusText(team.isPvpEnabled());
+                String message = plugin.getConfigManager().getMessage("actionbar.team_info")
+                    .replace("%team%", team.getName())
+                    .replace("%online%", String.valueOf(team.getOnlineCount()))
+                    .replace("%total%", String.valueOf(team.getMemberCount()))
+                    .replace("%pvp_status%", pvpStatus);
+                p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(plugin.getConfigManager().colorize(message)));
+            } else {
+                if (plugin.getConfig().getBoolean("settings.actionbar.enabled")) {
+                    String message = plugin.getConfigManager().getMessage("actionbar.no_team");
+                    p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(plugin.getConfigManager().colorize(message)));
+                }
+            }
+        }
+    }
+                  }
