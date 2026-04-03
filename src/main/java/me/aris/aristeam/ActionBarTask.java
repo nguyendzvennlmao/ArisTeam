095ec80c0bdeab98c076b60ad84a005c9875a9a5
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-03 09:14:25 +0700

    Update ActionBarTask.java

--- src/main/java/me/aris/aristeam/ActionBarTask.java
+++ src/main/java/me/aris/aristeam/ActionBarTask.java
@@ -1,18 +1,16 @@
 package me.aris.aristeam;
 
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
-public class ActionBarTask extends BukkitRunnable {
+public class ActionBarTask {
     private ArisTeams plugin;
 
     public ActionBarTask(ArisTeams plugin) {
         this.plugin = plugin;
     }
 
-    @Override
     public void run() {
         if (!plugin.getConfig().getBoolean("settings.actionbar.enabled")) return;
         
@@ -36,4 +34,4 @@ public class ActionBarTask extends BukkitRunnable {
             }
         }
     }
-}
+                        }
